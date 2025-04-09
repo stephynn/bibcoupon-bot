0da214cc853c0784c3d49d3fbf150364f0c9b0a4
stephynn <stehanieneo@gmail.com>
2025-04-10 00:08:31 +0800

    codes

---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+load_dotenv()  
+
+API_TOKEN = os.getenv('BOT_TOKEN')
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Create 250 coupons
+coupon_pool = [f'bibxatrizstudio-{str(i).zfill(3)}' for i in range(1, 251)]
+available_coupons = set(coupon_pool)
+user_coupons = {}
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton("Generate a Coupon Code", callback_data="generate_coupon"))
+    bot.send_message(message.chat.id, "Welcome! Click the button below to get your coupon code.", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: call.data == "generate_coupon")
+def generate_coupon(call):
+    user_id = call.from_user.id
+
+    if user_id in user_coupons:
+        bot.answer_callback_query(call.id, "You already generated your coupon code!")
+        coupon = user_coupons[user_id]
+    elif available_coupons:
+        coupon = available_coupons.pop()
+        user_coupons[user_id] = coupon
+        bot.answer_callback_query(call.id, "Coupon generated successfully!")
+    else:
+        bot.answer_callback_query(call.id, "All coupon codes have been used.")
+        bot.send_message(call.message.chat.id, "Sorry, all coupons are taken.")
+        return
+
+    # Instagram button
+    markup = InlineKeyboardMarkup()
+    markup.add(InlineKeyboardButton("💐 Visit our Instagram", url="https://www.instagram.com/bunnyinblooms/"))
+
+    # Send coupon with Instagram button
+    bot.send_message(call.message.chat.id, f"🎉 Your coupon code is: {coupon}", reply_markup=markup)
+
+# Run the bot
+bot.polling()
